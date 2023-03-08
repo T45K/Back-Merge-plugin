c458c60104a156ad1693a8d3746e3244cc338800
T45K <tasktas9@gmail.com>
2023-03-08 18:59:56 +0900

    modify impl

--- src/main/kotlin/io/jenkins/plugins/back_merge/EntryPoint.kt
+++ src/main/kotlin/io/jenkins/plugins/back_merge/EntryPoint.kt
@@ -3,12 +3,14 @@ package io.jenkins.plugins.back_merge
 import io.jenkins.plugins.back_merge.domain.UrlElements
 import io.jenkins.plugins.back_merge.infrastructure.HttpClientImpl
 import io.jenkins.plugins.back_merge.usecase.BackMergeUsecase
+import io.jenkins.plugins.back_merge.util.AuthorizationCredentialFactory
 import java.io.PrintStream
 
 class EntryPoint(
     private val logger: PrintStream,
     private val basicAuthUserName: String,
     private val basicAuthUserPassword: String,
+    private val httpAccessToken: String,
     private val gitRepositoryHostingServiceUrl: String,
     private val projectName: String,
     private val repositoryName: String,
@@ -16,7 +18,9 @@ class EntryPoint(
 ) {
     fun main() {
         JenkinsLogger.delegate = logger
-        val httpClient = HttpClientImpl(basicAuthUserName, basicAuthUserPassword)
+        val credential =
+            AuthorizationCredentialFactory.create(basicAuthUserName, basicAuthUserPassword, httpAccessToken)
+        val httpClient = HttpClientImpl(credential)
         val usecase = BackMergeUsecase(httpClient)
         val urlElements = UrlElements(gitRepositoryHostingServiceUrl, projectName, repositoryName)
 
